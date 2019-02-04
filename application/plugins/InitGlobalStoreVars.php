506acb3551ddc7c6f76dfd049ac42543c0cee45f
Ronald von Schlegell <rvs75@email.de>
2019-02-04 10:51:43 +0100

    revert

--- application/plugins/InitGlobalStoreVars.php
+++ application/plugins/InitGlobalStoreVars.php
@@ -40,7 +40,7 @@ class Application_Plugin_InitGlobalStoreVars extends Zend_Controller_Plugin_Abst
         $storeConfigName = $this->getStoreConfigName($storeHost);
         Zend_Registry::set('store_config_name', $storeConfigName);
 
-        //Zend_Registry::set('store_template', $this->getStoreTemplate($storeConfigName));
+        Zend_Registry::set('store_template', $this->getStoreTemplate($storeConfigName));
 
         $config_store = $this->getConfigStore($storeHost);
         Zend_Registry::set('store_config', $config_store);
