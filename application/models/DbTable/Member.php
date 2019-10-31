27f31ebb8e7d806c86b6aeb7e88ac22a90e66ff0
Ronald von Schlegell <rvs75@email.de>
2019-10-31 15:37:13 +0100

    auth

--- application/models/DbTable/Member.php
+++ application/models/DbTable/Member.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- *  ocs-apiserver
+ *  ocs-webserver
  *
  *  Copyright 2016 by pling GmbH.
  *
- *    This file is part of ocs-apiserver.
+ *    This file is part of ocs-webserver.
  *
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU Affero General Public License as
@@ -20,7 +20,7 @@
  *    You should have received a copy of the GNU Affero General Public License
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  **/
-class Application_Model_DbTable_Member extends Local_Model_Table
+class Default_Model_DbTable_Member extends Local_Model_Table
 {
 
     const MEMBER_ACTIVE = 1;
@@ -36,30 +36,33 @@ class Application_Model_DbTable_Member extends Local_Model_Table
     const MEMBER_DEFAULT_PROFILE_IMAGE = '/images/system/default-profile.png';
     const MEMBER_TYPE_GROUP = 1;
     const MEMBER_TYPE_PERSON = 0;
+    const ROLE_ID_MODERATOR = 400;
     const ROLE_ID_DEFAULT = 300;
     const ROLE_ID_STAFF = 200;
     const ROLE_ID_ADMIN = 100;
     const PROFILE_IMG_SRC_LOCAL = 'local';
     const SOURCE_LOCAL = 0;
     const SOURCE_HIVE = 1;
-
+    const PASSWORD_TYPE_OCS = 0;
+    const PASSWORD_TYPE_HIVE = 1;
+    const MEMBER_AVATAR_TYPE_USERUPDATED = 2;
     protected $_keyColumnsForRow = array('member_id');
 
     protected $_key = 'member_id';
 
     protected $_name = "member";
 
-    protected $_dependentTables = array('Application_Model_Project');
+    protected $_dependentTables = array('Default_Model_Project');
 
     protected $_referenceMap = array(
         'Owner' => array(
             'columns'       => 'member_id',
-            'refTableClass' => 'Application_Model_Project',
+            'refTableClass' => 'Default_Model_Project',
             'refColumns'    => 'member_id'
         ),
         'Email' => array(
             'columns'       => 'member_id',
-            'refTableClass' => 'Application_Model_DbTable_MemberEmail',
+            'refTableClass' => 'Default_Model_DbTable_MemberEmail',
             'refColums'     => 'email_member_id'
         )
     );
